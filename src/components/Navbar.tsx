32ff7e48c9e211d1ff36650c2f09d19ffe98fbaf
Ömer YAVAŞ <omeryavas.pro@gmail.com>
2022-12-18 13:04:26 +0300

    some small update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,41 +18,40 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className={classes.navbarBox}>
+      <div className={classes.navbarkutu}>
         <div>
           <a href="/">
             <img
               className={classes.navbarLogo}
-              srcSet="https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-500.png 500w, https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-800.png 800w, https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-1080.png 1080w, https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-1600.png 1600w, https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-2000.png 2000w, https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9.png 2562w"
-              src="https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9.png"
-              loading="lazy"
+              srcSet="https://uploads-ssl.webflow.com/63207aa6a0b066c193c93a03/6340f11a6a18b6834bb49612_Logo9-p-500.png 500w"
               alt="Company logo"
             />
           </a>
         </div>
-
-        <nav className={classes.navMenu}>
-          <div>
-            <NavLink activeClassName={classes.active} to="/home">
-              Home
-            </NavLink>
-          </div>
-          <div>
-            <NavLink activeClassName={classes.active} to="/featurepage">
-              Features
-            </NavLink>
-          </div>
-          <div>
-            <NavLink activeClassName={classes.active} to="/aboutus">
-              About us
-            </NavLink>
-          </div>
-          <div>
-            <NavLink activeClassName={classes.active} to="/contact">
-              Contact us
-            </NavLink>
-          </div>
-        </nav>
+        <div>
+          <nav className={classes.navMenu}>
+            <div>
+              <NavLink activeClassName={classes.active} to="/home">
+                Home
+              </NavLink>
+            </div>
+            <div>
+              <NavLink activeClassName={classes.active} to="/featurepage">
+                Features
+              </NavLink>
+            </div>
+            <div>
+              <NavLink activeClassName={classes.active} to="/aboutus">
+                About us
+              </NavLink>
+            </div>
+            <div>
+              <NavLink activeClassName={classes.active} to="/contact">
+                Contact us
+              </NavLink>
+            </div>
+          </nav>
+        </div>
 
         <div>
           <Link className={classes.navButton} to="/landingpage">
